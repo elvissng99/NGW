c9a2bcb840e6fb74d4d4dd57efd935accb1139dd
Jun Han <ongjunhan235@gmail.com>
2022-12-04 20:46:38 +0800

    Updated with RDF link

--- app.js
+++ app.js
@@ -8,19 +8,108 @@ const $rdf = require('rdflib')
 //eventually nid read from foaf file
 // const foafProfile = fs.readFileSync('foafProfile.ttl').toString()
 
-//hardcoded profile for now
-user_profile = {
-    name: "elvis",
-    email: "emailelvis@gmail.com",
-    interest:{
-        genre:["Comedy", "Superhero","Thriller"],
-        actor:["Tom Holland", "Tom Cruise"],
-        country:["United States of America", "South Korea"]
+const people = fs.readFileSync('people.ttl').toString()
+
+//console.log(user_profile)
+
+const store = $rdf.graph()
+
+$rdf.parse(
+	people,
+	store,
+	"http://userprofile.com/owl/profile",
+	"text/turtle"
+)
+
+//console.log(store)
+
+var userprofile = $rdf.sym('http://userprofile.com/owl/profile#UserProfile')
+var interest = $rdf.sym('http://userprofile.com/owl/profile#Interest')
+var hasinterest = $rdf.sym('http://userprofile.com/owl/profile#hasInterest')
+var name = $rdf.sym('http://userprofile.com/owl/profile#name')
+var email = $rdf.sym('http://userprofile.com/owl/profile#email')
+var movieyearstart = $rdf.sym('http://userprofile.com/owl/profile#movieYearStart')
+var movieyearend = $rdf.sym('http://userprofile.com/owl/profile#movieYearEnd')
+var genre = $rdf.sym('http://userprofile.com/owl/profile#genre')
+var actor = $rdf.sym('http://userprofile.com/owl/profile#actor')
+var elvis_interest = $rdf.sym('http://userprofile.com/owl/profile#elvis_interest')
+var country = $rdf.sym('http://userprofile.com/owl/profile#country')
+var elvis = $rdf.sym('http://userprofile.com/owl/profile#elvis')
+
+var elcountry = store.match(elvis_interest, country)
+var elgenre = store.match(elvis_interest, genre)
+var elactor = store.match(elvis_interest, actor)
+
+ for (let i=0; i<elgenre.length; i++){
+     console.log(elgenre[i].object.value)
+ }
+
+ var elname = store.match(elvis, name)
+ var elemail = store.match(elvis, email)
+ var elmovieyearstart = store.match(elvis, movieyearstart)
+ var elmovieyearend = store.match(elvis, movieyearend)
+
+ console.log(elname)
+
+ console.log(elname[0].object.value)
+
+ user_profile = {
+    name: elname[0].object.value,
+    email: elemail[0].object.value,
+    interest: {
+        genre:[],
+        actor:[],
+        country:[]
     },
-    movieYearRange:[2020,2022],
+    movieyearstart: elmovieyearstart[0].object.value,
+    movieyearend: elmovieyearend[0].object.value
+ }
+
+ for (let i=0; i<elgenre.length; i++){
+    user_profile.interest.genre.push(elgenre[i].object.value)
 }
 
+for (let i=0; i<elcountry.length; i++){
+    user_profile.interest.country.push(elcountry[i].object.value)
+}
+
+for (let i=0; i<elactor.length; i++){
+    user_profile.interest.actor.push(elactor[i].object.value)
+}
+
+ console.log(user_profile)
+
+
+
+// console.log(elcountry[0].object.value)
+
+// console.log(elcountry.length)
+
+// for (let i=0; i<elcountry.length; i++){
+//     console.log(elcountry[i].object.value)
+// }
+
+//console.log(store.statements[store.statements.length-1].object.value)
+
+
+
+//hardcoded profile for now
+// user_profile = {
+//     name: "elvis",
+//     email: "emailelvis@gmail.com",
+//     interest:{
+//         genre:["Comedy", "Superhero","Thriller"],
+//         actor:["Tom Holland", "Tom Cruise"],
+//         country:["United States of America", "South Korea"]
+//     },
+//     movieYearRange:[2020,2022],
+// }
+
 if(user_profile.interest.actor.length >0) user_profile.actorLinks = {};
+
+// if(elactor.length > 0){
+//     var actorLinks = {};
+// }
 
 // const store = $rdf.graph()
 // $rdf.parse(
@@ -30,18 +119,18 @@ if(user_profile.interest.actor.length >0) user_profile.actorLinks = {};
 // 	"text/turtle"
 // )
 
-const stringQuery = `
-	SELECT
-		?id
-		?name
-		?description
-	WHERE {
-		?game a <http://gameverse.com/owl/games#Game> .
-		?game <http://gameverse.com/owl/games#id> ?id .
-		?game <http://gameverse.com/owl/games#name> ?name .
-		?game <http://gameverse.com/owl/games#description> ?description .
-	}
-`
+// const stringQuery = `
+// 	SELECT
+// 		?id
+// 		?name
+// 		?description
+// 	WHERE {
+// 		?game a <http://gameverse.com/owl/games#Game> .
+// 		?game <http://gameverse.com/owl/games#id> ?id .
+// 		?game <http://gameverse.com/owl/games#name> ?name .
+// 		?game <http://gameverse.com/owl/games#description> ?description .
+// 	}
+// `
 
 // const query = $rdf.SPARQLToQuery(stringQuery, false, store)
 
@@ -62,6 +151,23 @@ const clientDBPedia = new ParsingClient({
 	endpointUrl: 'https://dbpedia.org/sparql'
 })
 	
+// for(let i = 0; i <elactor.length; i++){
+//     console.log(elactor[i].object.value)
+//     let query = "SELECT ?movie ?name WHERE { ?movie rdf:type dbo:Film . ?movie dbo:starring ?actor. ?actor dbp:name \""+elactor[i].object.value+"\"@en. ?movie dbp:name ?name }"
+//     clientDBPedia.query.select(query).then(result => {
+    
+//         console.log(result)
+//         actorLinks[elactor[i].object.value] = []
+//         result.forEach(row => {
+//             let movie = {[row.name.value]:row.movie.value}
+//             actorLinks[elactor[i].object.value].push(movie)
+//         })
+//         // console.log(user_profile)
+//     }).catch(error => {
+//         console.log(error)
+//     })
+// }
+
 for(let i = 0; i <user_profile.interest.actor.length; i++){
     let query = "SELECT ?movie ?name WHERE { ?movie rdf:type dbo:Film . ?movie dbo:starring ?actor. ?actor dbp:name \""+user_profile.interest.actor[i]+"\"@en. ?movie dbp:name ?name }"
     clientDBPedia.query.select(query).then(result => {
@@ -77,6 +183,14 @@ for(let i = 0; i <user_profile.interest.actor.length; i++){
         console.log(error)
     })
 }
+
+// const stringQuery = "SELECT ?movie ?name WHERE { ?movie rdf:type dbo:Film . ?movie dbo:starring ?actor. ?actor dbp:name \""+user_profile.interest.actor[i]+"\"@en. ?movie dbp:name ?name }"
+
+// const query = $rdf.SPARQLToQuery(stringQuery, false, store)
+
+// const stringQuery = "SELECT ?movie ?name WHERE { ?movie rdf:type dbo:Film . ?movie dbo:starring ?actor. ?actor dbp:name \""+elactor[i].object.value+"\"@en. ?movie dbp:name ?name }"
+
+// const query = $rdf.SPARQLToQuery(stringQuery, false, store)
 
 // const query = `
 //     SELECT ?movie
@@ -186,7 +300,7 @@ app.post('/query',function(request,response){
     }
     query += "?item wdt:P577 ?date."
     query += "SERVICE wikibase:label { bd:serviceParam wikibase:language \"[AUTO_LANGUAGE],en\". }."
-    query += "FILTER(xsd:integer(YEAR(?date)) >="+user_profile.movieYearRange[0]+" && xsd:integer(YEAR(?date)) <="+user_profile.movieYearRange[1] +")"
+    query += "FILTER(xsd:integer(YEAR(?date)) >="+user_profile.movieyearstart+" && xsd:integer(YEAR(?date)) <="+user_profile.movieyearend +")"
     query += "} LIMIT 10"
 
     // console.log(query)
